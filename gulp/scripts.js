07bf684d433e658b44069f5c2d1dcdc56bbb2c6e
Wilson Silva <wilson.dsigns@gmail.com>
2015-11-04 22:24:18 +0000

    Delegate linting to jshint task

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -8,10 +8,8 @@ var browserSync = require('browser-sync');
 
 var $ = require('gulp-load-plugins')();
 
-gulp.task('scripts', function () {
+gulp.task('scripts', ['jshint'], function () {
   return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
     .pipe(browserSync.reload({ stream: true }))
     .pipe($.size())
 });
